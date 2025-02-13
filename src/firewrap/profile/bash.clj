a5376020b868d5d9fb2521e768b65e38bdf9b072
Jakub Dundalek <dundalek@gmail.com>
2025-02-13 12:15:40 +0100

    bash profile

---
+++ src/firewrap/profile/bash.clj
@@ -0,0 +1,10 @@
+(ns firewrap.profile.bash
+  (:require
+   [firewrap.preset.base :as base]
+   [firewrap.preset.oldsystem :as system]))
+
+(defn profile [_]
+  (-> (base/base)
+      (system/libs)
+      ; (system/bind-ro-try "/dev/tty")
+      (system/command "bash")))
