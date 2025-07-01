99bf6f7b27c7de6ca07aa3ac064c998b61a67e1c
Jakub Dundalek <dundalek@gmail.com>
2025-07-01 11:41:29 +0200

    remove .clojure from claude

--- src/firewrap/profile/claude.clj
+++ src/firewrap/profile/claude.clj
@@ -29,48 +29,53 @@
 
 (defn- common [ctx]
   (sb/$-> ctx
-      (base/configurable {:opts {:cwd true
-                                 :net true
-                                 :home "claude"}})
-      (sb/bind-ro-try "/etc/claude-code")
-      ;; bind claude files, alternative consider symlinking to sandbox dir
-      (sb/bind-rw-try (dumpster/home ctx ".claude"))
-      (sb/bind-rw-try (dumpster/home ctx ".claude.json"))
-      (sb/bind-rw-try (dumpster/home ctx ".config/claude"))
-      (sb/bind-rw-try (dumpster/home ctx ".cache/claude-cli-nodejs"))
-      ;; Installed via Bun
-      ;; have its own packages, install inside sandbox with:
-      ;; fw --profile claude -- bun install -g @anthropic-ai/claude-code
-      ;; (sb/bind-ro-try (str (dumpster/home ctx) "/.bun"))
-      ;; To use the usual name when committing
-      (sb/bind-ro-try (dumpster/home ctx ".gitconfig"))
-      ;; bind git common dir for worktrees
-      (bind-worktree-git)
-      ;; For development of lua plugins
-      (sb/env-pass "LUA_PATH")
-      ;; for IDE integration
-      (sb/env-pass-many ["CLAUDE_CODE_SSE_PORT" "ENABLE_IDE_INTEGRATION"])
-      (sb/share-pid)
-      ;; For clojure-mcp
-      (sb/bind-ro-try (dumpster/home ctx ".clojure"))
-      (sb/bind-ro-try (dumpster/home ctx "dotfiles/clojure"))
-      (system/command "clojure-mcp")))
+    (base/configurable {:opts {:cwd true
+                               :net true
+                               :home "claude"}})
+    (sb/bind-ro-try "/etc/claude-code")
+    ;; bind claude files, alternative consider symlinking to sandbox dir
+    (sb/bind-rw-try (dumpster/home ctx ".claude"))
+    (sb/bind-rw-try (dumpster/home ctx ".claude.json"))
+    (sb/bind-rw-try (dumpster/home ctx ".config/claude"))
+    (sb/bind-rw-try (dumpster/home ctx ".cache/claude-cli-nodejs"))
+    ;; Installed via Bun
+    ;; have its own packages, install inside sandbox with:
+    ;; fw --profile claude -- bun install -g @anthropic-ai/claude-code
+    ;; (sb/bind-ro-try (str (dumpster/home ctx) "/.bun"))
+    ;; To use the usual name when committing
+    (sb/bind-ro-try (dumpster/home ctx ".gitconfig"))
+    ;; bind git common dir for worktrees
+    (bind-worktree-git)
+    ;; For development of lua plugins
+    (sb/env-pass "LUA_PATH")
+    ;; for IDE integration
+    (sb/env-pass-many ["CLAUDE_CODE_SSE_PORT" "ENABLE_IDE_INTEGRATION"])
+    (sb/share-pid)
+    ;; For clojure-mcp
+    ;; can't ro bind whole .clojure, because clojure might want to write to .clojure/.cpcache
+    ; (sb/bind-ro-try (dumpster/home ctx ".clojure/deps.edn"))
+    ; (sb/bind-ro-try (dumpster/home ctx ".clojure/tools"))
+    ; (sb/symlink (dumpster/home ctx "dotfiles/clojure/deps.edn") (dumpster/home ctx ".clojure/deps.edn"))
+    ; (sb/symlink (dumpster/home ctx "dotfiles/clojure/tools") (dumpster/home ctx ".clojure/tools"))
+    ; (sb/bind-ro-try (dumpster/home ctx "dotfiles/clojure/deps.edn"))
+    ; (sb/bind-ro-try (dumpster/home ctx "dotfiles/clojure/tools"))
+    (system/command "clojure-mcp")))
 
 (defn wide [_]
   (sb/$-> (base/base4)
-      (common)))
+    (common)))
 
 (defn narrow [_]
   (sb/$-> (base/base)
-      (sb/env-pass-many env/allowed)
-      (base/bind-system-and-extra-programs)
-      (system/dev-null)
-      (system/dev-pts)
-      (system/locale)
-      (system/timezone)
-      (system/tmp)
-      ; (dumpster/shell-profile)
-      ; seems to use following, but works without:
-      ; `/proc/sys/vm/overcommit_memory` - `/proc/version_signature` - `/proc/meminfo` - `/proc/self`
-      ; (dumpster/proc)
-      (common)))
+    (sb/env-pass-many env/allowed)
+    (base/bind-system-and-extra-programs)
+    (system/dev-null)
+    (system/dev-pts)
+    (system/locale)
+    (system/timezone)
+    (system/tmp)
+    ; (dumpster/shell-profile)
+    ; seems to use following, but works without:
+    ; `/proc/sys/vm/overcommit_memory` - `/proc/version_signature` - `/proc/meminfo` - `/proc/self`
+    ; (dumpster/proc)
+    (common)))
